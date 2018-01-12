de654ae0fa05b4d49535439cca4b20b10cc6388a
Babichev Maxim <maksim.babichev95@gmail.com>
2018-01-12 10:54:04 +0300

    Update CData.php

--- src/XMLReader/CData.php
+++ src/XMLReader/CData.php
@@ -5,12 +5,4 @@ namespace Bavix\XMLReader;
 class CData extends Raw
 {
 
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        return '<![CDATA[ ' . parent::__toString() . ' ]]>';
-    }
-
 }
